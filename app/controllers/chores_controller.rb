532efd3c1eeac4f55f3c34c6592e242d472d96d7
Natalia Takahara <nana_takahara@hotmail.com>
2020-03-04 17:20:13 +0900

    table added, fixed seeds, removed period from chores

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -1,5 +1,7 @@
 class ChoresController < ApplicationController
   def index
-    @chores = Chore.all
+    @room_chores = RoomChore.where(period: Period.current).order(created_at: :asc)
+    @rooms = @room_chores.map { |room_chore| room_chore.room }.sort_by { |room| room.number }
   end
+
 end
