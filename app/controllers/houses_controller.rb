4fb977d211b4fa6ea9f37e95c61dbe1871f356da
richard lynch <richlynch2@gmail.com>
2020-03-05 18:30:19 +0900

    added whiteboard styling

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,5 +1,9 @@
 class HousesController < ApplicationController
   def show
+    if current_user.room.nil?
+      current_user.room = Room.last
+    end
+
     @room_chores = current_user.room.room_chores
     @whiteboard_message = WhiteboardMessage.new
     @messages = WhiteboardMessage.all
