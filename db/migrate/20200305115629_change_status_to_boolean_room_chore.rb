807b41cbedd9a40ba8ac99f9e9682ea21b5b2848
Arnaud Rottier <59455445+arnaudrottier@users.noreply.github.com>
2020-03-05 21:18:01 +0900

    Delete 20200305115629_change_status_to_boolean_room_chore.rb

--- db/migrate/20200305115629_change_status_to_boolean_room_chore.rb
+++
@@ -1,6 +0,0 @@
-class ChangeStatusToBooleanRoomChore < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :room_chores, :status
-    add_column :room_chores, :status, :boolean, default: "pending"
-  end
-end
