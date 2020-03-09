8792c1536c5d31101f24e4a94d5f10860b2f2aba
richard lynch <richlynch2@gmail.com>
2020-03-09 09:19:27 +0900

    working on backend logic

--- app/models/merit/point_rules.rb
+++ app/models/merit/point_rules.rb
@@ -16,15 +16,14 @@ module Merit
       score 10, :on => 'users#create' do |user|
         user.bio.present?
       end
+      #needs to speak with user/room_chore
+      #must add points if chore if complete WHEN period of time ends and new chore is assigned
+      #right now, points are being awarded for creating posts/reviewing
+      #I tried to screw around, but am lost on conditionals here
 
-      score 15, :on => 'reviews#create', :to => [:reviewer, :reviewed]
+      score 15, :on => 'chores#create', :to => [:reviewer, :reviewed]
 
-      score 20, :on => [
-        'comments#create',
-        'photos#create'
-      ]
-
-      score -10, :on => 'comments#destroy'
+      score -10, :on => 'chores#destroy'
     end
   end
 end
