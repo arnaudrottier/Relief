a19d5a99ae90f1120f9cecdb631a74f2fcf82cfb
richard lynch <richlynch2@gmail.com>
2020-03-09 14:33:23 +0900

    creating amenities card partial

--- app/models/amenity.rb
+++ app/models/amenity.rb
@@ -1,5 +1,5 @@
 class Amenity < ApplicationRecord
   has_many :bookings
   validates :name, presence: true
-  # validates :availability, presence: true
+  validates :availability, presence: true
 end
