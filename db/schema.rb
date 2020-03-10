2efa7756a881984af7854cfb481f7c762d3805a8
Arnaud Rottier <59455445+arnaudrottier@users.noreply.github.com>
2020-03-10 09:24:33 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -172,7 +172,6 @@ ActiveRecord::Schema.define(version: 2020_03_09_062918) do
     t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "post_date"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
