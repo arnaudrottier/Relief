39eac6db08e02f21f9820de680e153d574b4d686
richard lynch <richlynch2@gmail.com>
2020-03-11 17:52:54 +0900

    re-add room chore to houses controller

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -3,6 +3,7 @@ class HousesController < ApplicationController
     if current_user.room.nil?
       current_user.room = Room.last
     end
+    @room_chores = current_user.room.room_chores
 
     @whiteboard_message = WhiteboardMessage.new
     @messages = WhiteboardMessage.last(10)
