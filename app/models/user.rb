529c10a276d4bf6b797e8c4f089448f3f3f5d117
arnaud rottier <arnaud.rottier@gmail.com>
2020-03-11 12:07:01 +0900

    sign up redirecting to login, booking dates display amended

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,9 @@ class User < ApplicationRecord
   has_many :whiteboard_messages
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :validatable
+         # :registerable,
   # validates :first_name, presence: :true
   # validates :last_name, presence: :true
   has_one_attached :image
