0aa90f00ae709bf4efadc7cd7ad9e524640b5c04
richard lynch <richlynch2@gmail.com>
2020-03-11 17:43:16 +0900

    re-implemented whiteboard message

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -4,6 +4,8 @@ class HousesController < ApplicationController
       current_user.room = Room.last
     end
 
+    @room_chores = current_user.room.room_chores
+
     @whiteboard_message = WhiteboardMessage.new
     @messages = WhiteboardMessage.last(10)
     @amenities = Amenity.all
