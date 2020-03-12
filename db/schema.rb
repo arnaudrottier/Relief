5d2620f2a014af8ddbecfa1cf055afd6777ce2c0
William Nunn <wnunnvfx@gmail.com>
2020-03-12 19:44:19 +0900

    Fixed recurring schema column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_12_025019) do
+ActiveRecord::Schema.define(version: 2020_03_12_104201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -157,7 +157,6 @@ ActiveRecord::Schema.define(version: 2020_03_12_025019) do
     t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "post_date"
     t.bigint "user_id"
     t.index ["user_id"], name: "index_whiteboard_messages_on_user_id"
   end
