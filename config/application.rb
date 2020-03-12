ae9ffb1e3d4b263f2ffc05c0f3f2b632bbd89399
arnaud rottier <arnaud.rottier@gmail.com>
2020-03-12 21:40:25 +0900

    timezone tokyo added to the logic of greetings

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Relief
   class Application < Rails::Application
+    config.time_zone = "Asia/Tokyo"
     config.generators do |generate|
           generate.assets false
           generate.helper false
