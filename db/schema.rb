ffe320c2cdf37530561b1c048d4cf1be00dcea3a
William Nunn <wnunnvfx@gmail.com>
2020-03-12 10:20:06 +0900

    Box Shadows added

--- db/schema.rb
+++ db/schema.rb
@@ -156,6 +156,7 @@ ActiveRecord::Schema.define(version: 2020_03_10_070500) do
     t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "post_date"
     t.bigint "user_id"
     t.index ["user_id"], name: "index_whiteboard_messages_on_user_id"
   end
