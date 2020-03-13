3e0c56f1ef01796b965343794b45902c0e3adc21
Natalia Takahara <nana_takahara@hotmail.com>
2020-03-13 12:12:29 +0900

    do not show past booking, show month if your book start and end in different month

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,13 +4,14 @@ class Booking < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true
   default_scope { order(start_date: :desc) }
+  scope :booking_from_today, -> { where("end_date >= ?", Date.today).order(start_date: :desc) }
 
   def formatted_start_date
     start_date.strftime("%b %d")
   end
 
   def formatted_end_date
-    end_date.strftime("%d")
+    start_date.strftime("%b") == end_date.strftime("%b") ? end_date.strftime("%d") : end_date.strftime("%b %d")
   end
 
   def formatted_start_time
